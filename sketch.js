31a684a2d8f61714c770b053ca07c901e4cf9aed
SaiTheMetalhead <97501337+SaiTheMetalhead@users.noreply.github.com>
2022-01-15 11:47:53 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,19 @@
 var trex,trex_running
 var edges,ground,groundImage;
-var cloud,cloudImage
+var cloud,cloudImage;
+var obstacle,obstacle1,obstacle2,obstacle3,obstacle4,obstacle5,obstacle6
 var invisibleGround;
+var score;
 function preload (){
   trex_running=loadAnimation("trex1.png","trex3.png","trex4.png")
   groundImage=loadImage("ground2.png")
   cloudImage=loadImage("cloud.png")
+  obstacle1=loadImage("obstacle1.png")
+  obstacle2=loadImage("obstacle2.png")
+  obstacle3=loadImage("obstacle3.png")
+  obstacle4=loadImage("obstacle4.png")
+  obstacle5=loadImage("obstacle5.png")
+  obstacle6=loadImage("obstacle6.png")
 }
 function setup (){
   createCanvas(600,200) 
@@ -19,9 +27,12 @@ function setup (){
   edges=createEdgeSprites();
   //var run=Math.round(random(10,60))
   //console.log(run)
+  score=0;
 }
 function draw(){
   background("white")
+  text("Score: "+score,500,50)
+  score=score+Math.round(frameCount/60)
   if(keyDown("space")&& trex.y>160){
     trex.velocityY=-8
   }
@@ -32,6 +43,7 @@ function draw(){
   }
   trex.velocityY=trex.velocityY+0.5;
   trex.collide(invisibleGround)
+  spawnObstacles();
 spawnClouds();
   drawSprites();
 }
@@ -41,7 +53,33 @@ function spawnClouds(){
     cloud.addImage(cloudImage)
     cloud.scale=1
     cloud.velocityX=-3
+    cloud.lifetime=200
     cloud.depth=trex.depth;
     trex.depth=trex.depth+1
   }
+}
+function spawnObstacles(){
+if(frameCount%60===0){
+  obstacle=createSprite(600,165,10,40)
+  obstacle.velocityX=-6
+  var rand=Math.round(random(1,6))
+  switch(rand){
+    case 1:obstacle.addImage(obstacle1);
+    break;
+    case 2:obstacle.addImage(obstacle2);
+    break;
+    case 3:obstacle.addImage(obstacle3);
+    break;
+    case 4:obstacle.addImage(obstacle4);
+    break;
+    case 5:obstacle.addImage(obstacle5);
+    break;
+    case 6:obstacle.addImage(obstacle6);
+    break;
+    default:break
+  }
+  obstacle.lifetime=100
+  obstacle.scale=0.5
+}
+
 }
